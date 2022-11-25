571ad33e820b03dd366fc4fdc32423811ccfeebd
Cole Frank <cole.v.frank@gmail.com>
2022-11-25 17:56:12 -0500

    renamed, new docstring for compute_basic_weight_Stats

--- src/analyze_bert_base.py
+++ src/analyze_bert_base.py
@@ -14,6 +14,10 @@ def analyze_bert_ffn(model, layer_num):
             model.encoder.layer[0].attention.self.query.weight.detach().numpy())
     #query, key, value weights
 
+
+
+def compute_ffn_weight_statistics:
+
 def analyze_bert_self_attn(model, layer_num):
     layer = model.encoder.layer[layer_num]
     query_weights = layer.attention.self.query.weight.data.numpy()
@@ -22,18 +26,30 @@ def analyze_bert_self_attn(model, layer_num):
     attn_output = layer. attention.output.dense.weight.data.numpy()
     
     
-    q_out_df, q_in_df = attn_weight_statistics(query_weights)
-    k_out_df, k_in_df = attn_weight_statistics(key_weights)
-    v_out_df, v_in_df = attn_weight_statistics(value_weights)
-    ffn_out_df, ffn_in_df = attn_weight_statistics(attn_output)
+    q_out_df, q_in_df = compute_basic_weight_stats(query_weights)
+    k_out_df, k_in_df = compute_basic_weight_stats(key_weights)
+    v_out_df, v_in_df = compute_basic_weight_stats(value_weights)
+    ffn_out_df, ffn_in_df = compute_basic_weight_stats(attn_output)
 
     q_dead_out_nodes = sum(q_out_df['nonzero'] == 0)
     q_dead_in_nodes = sum(q_in_df['nonzero'] == 0)
+    #TODO fill out the rest of this functio for key, value, ffn
     
     
     
-def attn_weight_statistics(param_array):
-    
+def compute_basic_weight_stats(param_array):
+    """
+    Function for computing basic weight statistics of a single parameter array
+
+    Input
+        param_array (numpy array) : two dimensional array of weights
+
+    Return
+        outgoing_df (pandas df) : two dimensional df of stats for weights
+                                    corresponding to inputs to the param_array
+        incoming_df (pandas df) : two dimensional df of stats for weights
+                                    corresponding to outputs of the param_array
+    """
     # number of non-zero weights per input dimension
     nonzero_outgoing = (param_array != 0).sum(axis=0)
     # number of non-zero weights per output dimension
